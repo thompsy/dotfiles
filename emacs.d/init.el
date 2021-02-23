a858368e530e4a05c91db00102e9527c42b8ddac
Andrew Thompson <thompsy@downthewire.co.uk>
2021-02-23 15:00:02 +0000

    Use UTF-8 by default

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -158,6 +158,18 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; Set locale to UTF-8
+;;
+
+(set-language-environment 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;; Backups
 ;;
 (setq
